6e78edbbd31be939a9d3ca6fcda9476f3043b3b5
Martin Malek <martin.malek@softhouse.se>
2015-03-27 17:18:45 +0100

    Calculate competences per office

--- app/controllers/wordcloud_controller.rb
+++ app/controllers/wordcloud_controller.rb
@@ -3,12 +3,34 @@ class WordcloudController < ApplicationController
   def render_wordcloud
     # TODO: Limit to user scope. Need connectin to User to work
     @competences = Wordcloud.getAllUserComptences
+    city_competences = Hash.new()
+
+    json = JSON.parse(@competences)
+
+    json.each do |user|
+        puts user['office']
+        if(!city_competences.has_key?(user['office']))
+            city_competences[user['office']] = Hash.new()
+        end
+
+        user['competences'].each do |competence|
+            if(!city_competences[user['office']].has_key?(competence['type']))
+                city_competences[user['office']][competence['type']] = 1
+            else
+                city_competences[user['office']][competence['type']] += 1
+            end
+        end
+
+    end
+
+    # Create all cities
+
+    # Filter on city
 
     respond_to do |format|
-      format.html { render html: @competences }
-      format.json { render json: @competences }
+      format.html { render html: city_competences }
+      format.json { render json: city_competences }
     end
   end
-
 
 end
